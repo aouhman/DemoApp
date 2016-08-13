de06ff6db51730af25dc6e5eb112f254d4c4f318
aouhman <abdessamad.ouhman@gmail.com>
2016-08-13 20:54:05 +0000

    ajouter 2 methode au controller pour incrementer  vote et le desincrementer

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,6 +2,14 @@
 
 eventsApp.controller('EventController',
     function ($scope) {
+
+        $scope.upVoteSession = function (session) {
+             session.upVoteCount++;
+        };
+        $scope.downVoteSession = function (session) {
+            session.upVoteCount--;
+        };
+
         $scope.event = {
             name: 'Angular Boot camp',
             date: '1/1/2013',
@@ -18,21 +26,24 @@ eventsApp.controller('EventController',
                     creatorName: 'Bob smith',
                     duration: '1 hr',
                     level: 'Advance',
-                    abstract: 'In this session you will learn the ins and outs of directives!!'
+                    abstract: 'In this session you will learn the ins and outs of directives!!',
+                    upVoteCount: 0
                 },
                 {
                     name: 'Directives Masterclass',
                     creatorName: 'john Doe',
                     duration: '30 mins',
                     level: 'Introduction',
-                    abstract: 'This session will take a closer look at scopes '
+                    abstract: 'This session will take a closer look at scopes ',
+                    upVoteCount: 0
                 },
                 {
                     name: 'Well Behaved Controllers',
                     creatorName: 'Jane Doe',
                     duration: '2 hours',
                     level: 'Intermediate',
-                    abstract: 'Controller are the beginning of everything Angular'
+                    abstract: 'Controller are the beginning of everything Angular',
+                    upVoteCount: 0
                 }
             ]
         }
