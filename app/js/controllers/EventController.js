2231d1b8f03d1115933ff1c9b386eeaa3ef9902c
aouhman <abdessamad.ouhman@gmail.com>
2016-08-14 19:33:51 +0000

    ajouter  un service a l'application eventData

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,51 +1,14 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function ($scope) {
+    function ($scope, eventData) {
 
         $scope.sniffer = "<span style='color: red'>h1 there</span>";
         $scope.boolValue = false;
         $scope.myclass = "background-color:blue";
         $scope.buttonDisabled = true;
         $scope.sortorder = '-upVoteCount';
-
-            $scope.event = {
-                name: 'Angular Boot camp',
-                date: '1/1/2013',
-                time: '10:30 am',
-                imageUrl: '/img/angularjs-logo.png',
-                location: {
-                    address: 'Morocco',
-                    city: 'casablanca ',
-                    province: 'Ds '
-                },
-                sessions: [
-                    {
-                        name: 'Directives Masterclass Introductory',
-                        creatorName: 'Bob smith',
-                        duration: 1,
-                        level: 'advance',
-                        abstract: 'In this session you will learn the ins and outs of directives!!',
-                        upVoteCount: 0
-                    },
-                    {
-                        name: 'Directives Masterclass',
-                        creatorName: 'john Doe',
-                        duration: 2,
-                        level: 'introductory',
-                        abstract: 'This session will take a closer look at scopes ',
-                        upVoteCount: 0
-                    },
-                    {
-                        name: 'Well Behaved Controllers',
-                        creatorName: 'Jane Doe',
-                        duration: 4,
-                        level: 'intermediate',
-                        abstract: 'Controller are the beginning of everything Angular',
-                        upVoteCount: 0
-                    }
-                ]
-            };
+        $scope.event = eventData.event;
 
         $scope.upVoteSession = function (session) {
             session.upVoteCount++;
