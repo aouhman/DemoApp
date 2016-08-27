e99d1c3806b6ff82d2f32c7bd42e38d932203302
aouhman <abdessamad.ouhman@gmail.com>
2016-08-27 20:46:24 +0000

    SampleDirective other définition

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,9 @@
 
 eventsApp.directive('mySample', function ($compile) {
     return {
-        link: function (scope, element, attrs, controller) {
-            var markup = "<input type='text' ng-model='sampleDate'/>{{sampleDate}}<br/>";
-            angular.element(element).html($compile(markup)(scope));
-        }
+        restrict: "E",
+        template: "<input type='text' ng-model='sampleDate'/>{{sampleDate}}<br/>"
+
+
     }
 });
