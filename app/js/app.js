71e2550de777d290e351eb77db2baa1da5c656b2
aouhman <abdessamad.ouhman@gmail.com>
2016-08-27 12:11:37 +0000

    SampleDirective creation de directive

--- app/js/app.js
+++ app/js/app.js
@@ -18,6 +18,12 @@ var eventsApp = angular.module('eventsApp', ['ngSanitize','ngResource' ,'ngRoute
                 template: 'Hello World',
                 controller: 'EventController'
             });
+        $routeProvider.when('/sampleDirective',
+            {
+                templateUrl: 'templates/SampleDirective.html',
+                controller: 'SampleDirectiveController'
+            });
+
         $routeProvider.otherwise({redirectTo:'/events'});
         $locationProvider.html5Mode(true);
     });
--- app/js/directives.js
+++ app/js/directives.js
@@ -1,2 +1,11 @@
 'use strict';
 
+
+eventsApp.directive('mySample', function ($compile) {
+    return {
+        link: function (scope, element, attrs, controller) {
+            var markup = "<input type='text' ng-model='sampleDate'/>{{sampleDate}}<br/>";
+            angular.element(element).html($compile(markup)(scope));
+        }
+    }
+});
